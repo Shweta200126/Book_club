4f0405648131a7997d855865c66c636800cbd99a
Shweta <72426535+Shweta200126@users.noreply.github.com>
2021-01-13 01:39:33 +0530

    Create contactus.php

---
+++ contactus.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>
+Contact Us
+</title>
+</head>
+<body bgcolor="lavender">
+<center>
+<font color="blue" face="Jokerman"><h1>WELCOME TO BOOK CLUB</h1></font>
+<br>
+Words leave imprints in your mind like footprints in the sand.
+<hr color=purple size=3>
+<table cellspacing=10 cellpadding=10 >
+<tr>
+<td><a href="booksadd.php"> HOME</a>
+<td><a href="info.php">INFORMATION</a>
+<td><a href="booksform.php">ADD BOOK RECORD</a>
+<td><a href="bookslogin.php">LOGIN</a>
+<td><a href="contactus.php">CONTACT US</a>
+</table>
+<hr color=purple size=3>
+<embed src="book2.mp4" LOOP="true" autostart="true" height=200 width=200>
+<p>
+A BOOKS & CO Intiative.
+<p><u><b>Contact Us at:</b></u>
+<p>
+203H,1<sup>st</sup> Floor<br>
+Bhikaji Cama Place<br>
+RK Puram, New Delhi-110012<br>
+Ph no :011-23456789-90
+<p>
+Email: <a href="mailto:contactus@booksclub.com">contactus@booksclub.com</a>
+<p>
+<img src="card.png" height=100 width=200 align=right>
+</body>
+</html>
