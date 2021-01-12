3403d2c366a20fee105863e1c8ae55c30a1c42dd
Shweta <72426535+Shweta200126@users.noreply.github.com>
2021-01-13 01:34:03 +0530

    Create info.php

---
+++ info.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+<body bgcolor=lavender>
+<center>
+<font color="blue" face="Jokerman"><h1>WELCOME TO  BOOK CLUB</h1></font>
+<br>
+Words leave imprints in your mind like footprints in the  sand.
+<hr color=purple size=3>
+<table cellspacing=5 cellpadding=10>
+<tr>
+<td><a href="booksadd.php"> HOME</a>
+<td><a href="info.php">INFORMATION</a>
+<td><a href="booksform.php">ADD BOOK RECORD</a>
+<td><a href="bookslogin.php">LOGIN</a>
+<td><a href="contactus.php">CONTACT US</a>
+</table>
+</center>
+<hr color=purple size=3>
+<img src="quote.png" align=right height=60%  width=10%>
+Types of books:
+<ol>
+<li>FICTION:
+<ul type="disc">
+<li>Stories
+<ul type="square">
+<li>short stories
+<li>story series
+</ul>
+<li>Novels
+<li>Comics
+</ul>
+<br>
+<li>NON FICTION
+<ul type="disc">
+<li>Autobiography
+<li>Philosophical
+</ul>
+</ol>
+</body>
+</html>
+
